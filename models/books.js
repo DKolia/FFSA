f962c6ec7e9ea1525bec7d6449ad6320a26144d6
David Kolia <Dkolia@icloud.com>
2018-07-15 22:18:35 -0500

    Added additional packages, set up nodemon, added database data

---
+++ models/books.js
@@ -0,0 +1,18 @@
+const myBookshelf = [
+
+  {title: "Drinkers of the Wind",                                           isGift: true,  read: false},
+  {title: "Graphic Artists Guild Handbook to Pricing & Ethical Guidelines", isGift: false, read: false},
+  {title: "Obama's Legacy",                                                 isGift: true,  read: false},
+  {title: "Bark!",                                                          isGift: false, read: false },
+  {title: "AP Stylebook",                                                   isGift: false, read: false},
+  {title: "500 Cameras",                                                    isGift: true,  read: true},
+  {title: "You Can Read Anyone",                                            isGift: false, read: false},
+  {title: "The Subtle Art of Not Giving A F*ck",                            isGift: true,  read: false},
+  {title: "The Elements of Journalism",                                     isGift: false, read: false},
+  {title: "Spark Joy",                                                      isGift: false, read: true},
+  {title: "The Life Changing Magic of Tidying Up",                          isGift: false, read: true},
+  {title: "A Game of Thrones",                                              isGift: false, read: true}
+
+];
+
+// This is my bookshelf at home. I have sold or donated many, but this is a catalog of my Hall of Fame- so to speak.
